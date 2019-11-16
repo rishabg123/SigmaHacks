bc04e0e7e0e647f193173a3b8710968874b9a857
Rishab Gupta <rishabg2004@gmail.com>
2019-11-16 12:57:37 -0800

    Initial Commit

---
+++ SigmaHacks(studyApp)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SigmaHacks(studyApp)
+//
+//  Created by Rishab Gupta on 11/16/19.
+//  Copyright © 2019 Rishab Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
